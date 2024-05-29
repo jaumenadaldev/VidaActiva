c5ab5f47bf608ec98f2808f5f4718dc01da244bf
jaumenadaldev <jaumenadaldev@gmail.com>
2024-05-29 15:01:14 +0200

    primers canvis

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <h1>Vida Sana</h1>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
